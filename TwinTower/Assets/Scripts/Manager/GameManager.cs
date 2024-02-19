688527f72d79917421104dc42c1d112582efa3fb
kimmoonwoong <ansdnd1500@naver.com>
2024-02-19 20:14:39 +0900

    feature: 매니저 구조화

---
+++ TwinTower/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace TwinTower
+{
+    /// <summary>
+    /// GameManager 클래스입니다. 게임의 전반적인 진행을 관리합니다.
+    /// </summary>
+    public class GameManager : Manager<GameManager>
+    {
+        protected override void Awake()
+        {
+            
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        protected void FixedUpdate()
+        {
+            
+        }
+    }
+}
