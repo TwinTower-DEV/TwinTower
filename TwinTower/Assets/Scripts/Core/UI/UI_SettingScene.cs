452fa01040eab30db7a284ff5b3868e764810286
kimmoonwoong <ansdnd1500@naver.com>
2024-09-26 02:05:06 +0900

    commit

--- TwinTower/Assets/Scripts/Core/UI/UI_SettingScene.cs
+++ TwinTower/Assets/Scripts/Core/UI/UI_SettingScene.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using System;
-
 
 namespace TwinTower
 {
