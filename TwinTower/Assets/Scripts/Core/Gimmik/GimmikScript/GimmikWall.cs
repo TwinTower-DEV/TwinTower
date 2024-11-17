231fe99c3348c8278aecfff4d64863d42774d9c8
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2024-11-17 23:32:57 +0900

    벽 수정

--- TwinTower/Assets/Scripts/Core/Gimmik/GimmikScript/GimmikWall.cs
+++ TwinTower/Assets/Scripts/Core/Gimmik/GimmikScript/GimmikWall.cs
@@ -5,7 +5,9 @@ namespace TwinTower
 {
     public class GimmikWall : GimmikBase
     {
-        private void Awake() {
+        // GimmikWall은 Map에서 직접 생성해주고 있음. 따라서 생성자 이용하여 isWalkable 덮어쓰도록 구현함. Awake는 버그인지 동작 안해서 생성자로 바꿈
+        public GimmikWall()
+        {
             isWalkable = false;
         }
     }
